443a95a8fae4062b34a60d11e4034d25e3bc6f39
Peter Feuerer <peter@piie.net>
2014-03-02 04:56:26 +0100

    code cleanup + wpm speedup/slowdown buttons added

--- gritz.pl
+++ gritz.pl
@@ -25,93 +25,122 @@ use Pod::Usage;
 
 # defines
 my $font = "courier new 24";
+my $span_black_open = "<span background='white' foreground='black' font_desc='".$font."'><big>";
+my $span_red_open = "<span background='white' foreground='red' font_desc='".$font."'><big>";
+my $span_close = "</big></span>";
+my $word_width = 28;
 
-
-# global variables
+# globaly used gtk stuff
 my $gtk_text;
+my $gtk_speed_label;
 my $gtk_sentence_text;
 my $gtk_timer;
-my $timeout;
+
+# global variables
+my $wpm = 200;
 my $pause_button;
-my $pause = 0;
+my $pause = 1;
 my $back_ptr = -1;
 my $prev_back_ptr = -1;
-my $sentence_cnt = 0;
-my $fastforward = 0;
+my $fast_forward = 0;
 
 
-my @words;
-my @backbuffer;
-sub get_word
+####################
+# Helper functions #
+####################
+
+sub get_line
 {
 	my $line;
 	$/ = '.';
-	@words = () if ($back_ptr != $prev_back_ptr);
+
+	if (!($line = <FILE>)) {
+		printf("reached end of file\n");
+		close(FILE);
+		exit(-1);
+	}
+	$line =~ s/[\n\r]/ /g;
+
+	return $line;
+}
+
+my @words_buffer;
+my @back_buffer;
+my $sentence_cnt = 0;
+sub get_next_word
+{
+	my $line;
+
+	@words_buffer = () if ($back_ptr != $prev_back_ptr);
 	$back_ptr = -1 if ($back_ptr < -1);
 	$prev_back_ptr = $back_ptr;
 
-
 	$gtk_sentence_text->set_markup("sentence nr: ".($sentence_cnt - ($back_ptr +1)));
 
-	for (;$fastforward; $fastforward--) {
-		$line = <FILE>;
-		$line =~ s/[\n\r]/ /g;
-		@words = split(' ', $line);
-		$sentence_cnt++ if ($#words >= 0);
-		if ($fastforward < 10) {
-			unshift(@backbuffer, $line);
-			pop(@backbuffer) if ($#backbuffer > 10);
-			printf("new sentence: @words, buff: $#backbuffer\n");
+	# if fast foward is specified, search the line
+	for (;$fast_forward; $fast_forward--) {
+		$line = get_line();
+		@words_buffer = split(' ', $line);
+		$sentence_cnt++ if ($#words_buffer >= 0);
+		if ($fast_forward < 10) {
+			unshift(@back_buffer, $line);
+			pop(@back_buffer) if ($#back_buffer > 10);
 		}
 	}
 
 
+	# standard operation:
+	# check if new word is in @words_buffer otherwise get new line
+	# into @words_buffer and insert it in @back_buffer
 	if ($back_ptr <= -1) {
-		if ($#words < 0) {
-			while ($#words < 0) {
-				$line = <FILE>;
-				$line =~ s/[\n\r]/ /g;
-				@words = split(' ', $line);
+		if ($#words_buffer < 0) {
+			while ($#words_buffer < 0) {
+				$line = get_line();
+				@words_buffer = split(' ', $line);
 			}
 			$sentence_cnt++;
-			unshift(@backbuffer, $line);
-			pop(@backbuffer) if ($#backbuffer > 10);
-			printf("new sentence: @words, buff: $#backbuffer\n");
+			unshift(@back_buffer, $line);
+			pop(@back_buffer) if ($#back_buffer > 10);
 		}
 	} else {
-		if ($#words < 0) {
-			@words = split(' ', $backbuffer[$back_ptr]);
-			printf("old sentence: @words, buff: $#backbuffer\n");
+		# somebody is rewinding to previous lines, so put the previous
+		# line into @words_buffer
+		if ($#words_buffer < 0) {
+			@words_buffer = split(' ', $back_buffer[$back_ptr]);
 		}
-		$back_ptr-- if ($#words <= 0);
+		# if @words_buffer empty, proceed with next line
+		$back_ptr-- if ($#words_buffer <= 0);
 	}
 
-	return shift(@words);
+	return shift(@words_buffer);
 }
 
-sub quit
+#################
+# GTK callbacks #
+#################
+sub button_quit
 {
 	Gtk2->main_quit;
 	close(FILE);
 	return TRUE;
 }
 
-sub back
+sub button_back
 {
 	$back_ptr++ if ($back_ptr < 10);
 	return TRUE;
 }
 
-sub forward
+sub button_forward
 {
 	$back_ptr-- if ($back_ptr > -2);
 	return TRUE;
 }
 
-sub pause 
+sub button_pause 
 {
 	if ($pause) {
-		$gtk_timer = Glib::Timeout->add($timeout, \&set_text);
+		$gtk_timer = Glib::Timeout->add(500, \&set_text);
 		$pause = 0;
 		$pause_button->set_label(" || ");
 	} else {
@@ -121,56 +150,77 @@ sub pause
 	return TRUE;
 }
 
+sub button_slower
+{
+	$wpm -= 10 if ($wpm > 40);
+	$gtk_speed_label->set_markup("WPM: $wpm");
+	return TRUE;
+}
+
+sub button_faster
+{
+	$wpm += 10 if($wpm < 1000);
+	$gtk_speed_label->set_markup("WPM: $wpm");
+	return TRUE;
+}
+
+######################
+# GTK timer callback #
+######################
+
 sub set_text
 {
-	my $word = get_word();
+	my $word = get_next_word();
+	my $timeout = 60000 / $wpm;
 	my $next_shot = $timeout;
 	my $word_length = length($word);
 	my $word_start = "";
 	my $word_mid = "";
 	my $word_end = "";
-	my $span_black_open = "<span background='white' foreground='black' font_desc='".$font."'><big>";
-	my $span_red_open = "<span background='white' foreground='red' font_desc='".$font."'><big>";
-	my $span_close = "</big></span>";
 	my $prev_vowel = -1;
 	my $i = 0;
-	my $width = 28;
 	my $add_to_end = 0;
 
 	
-	#$next_shot += ($timeout / 30) * $word_length - 6 if ($word_length > 6);
+	# calculate timeout for next run
 	$next_shot += $timeout / 2 if ($word_length > 10);
 	$next_shot += $timeout / 2 if ($word_length > 14);
 	$next_shot += $timeout / 2 if ($word_length > 18);
 	$next_shot += $timeout * 1.5 if ($word =~ /.*[\.!?;]$/);
 	$next_shot += $timeout / 2 if ($word =~ /.*,$/);
 
+	# search for vowel from start to the mid of the word,
+	# this will be the focuspoint of the word
 	for ($i = 0; $i < $word_length / 2; ++$i) {
 		if (substr($word, $i, 1) =~ /[aeuioöäü]/i) {
 			$prev_vowel = $i;
 		}
 	}
 
+	# if no vowel was found in the first half of the word,
+	# use the letter in the middle as focuspoint
 	$prev_vowel = $word_length / 2 if ($prev_vowel == -1);
 
-	for ($i = 0; $i < ($width / 2) - $prev_vowel; ++$i)
-	{
+	# fill the start of the word with spaces, to correctly
+	# align it
+	for ($i = 0; $i < ($word_width / 2) - $prev_vowel; ++$i) {
 		$word_start .= " ";
 	}
 
 	$word_start .= substr($word, 0, $prev_vowel);
 	$word_mid = substr($word, $prev_vowel , 1);
 	$word_end = substr($word, $prev_vowel + 1);
-	$add_to_end = $width / 2  - length($word_end);
+	$add_to_end = $word_width / 2  - length($word_end);
 
-	for ($i = 0; $i < $add_to_end ; ++$i)
-	{
+	# fill the string to fit $word_width
+	for ($i = 0; $i < $add_to_end ; ++$i) {
 		$word_end .= " ";
 	}
 	$word = $span_black_open.$word_start.$span_close.$span_red_open.$word_mid.$span_close.$span_black_open.$word_end.$span_close;
 
-
 	$gtk_text->set_markup($word);
+
+	# set new timer / disable in case of pause
 	Glib::Source->remove($gtk_timer);
 	if (!$pause) {
 		$gtk_timer = Glib::Timeout->add($next_shot,\&set_text);
@@ -178,81 +228,117 @@ sub set_text
 	return TRUE;
 }
 
+########
+# main #
+########
+
 sub main
 {
 	my $window;
 	my $quit_button;
 	my $back_button;
 	my $forward_button;
+	my $faster_button;
+	my $slower_button;
 	my $vbox;
 	my $hbox;
-	my $wpm = 200;
 	my $file = "infile.txt";
 	my $length = 24;
 	my $help = 0;
 	my $man = 0;
+	my $word = "";
+	my $i;
 
+	# handle arguments
 	GetOptions (	"wpm|w=i" => \$wpm,
-			"fastforward|f=i" => \$fastforward,
+			"fastforward|f=i" => \$fast_forward,
 			"help|h" => \$help,
 			"man|m" => \$man)
 		or die("Error in command line arguments\n");
-
 	pod2usage(1) if $help;
 	pod2usage(-verbose => 2) if $man;
 	pod2usage("$0: No file given.")  if (@ARGV == 0);
 
-	$file = $ARGV[0];
-	printf("file: $file\n");
+	# limit wpm
+	$wpm = 40 if ($wpm < 40);
+	$wpm = 1000 if ($wpm > 1000);
 
+	# open file
+	$file = $ARGV[0];
+	printf("opening file: $file\n");
 	open(FILE, "<:encoding(UTF-8)", $file) || die "can't open UTF-8 encoded filename: $!";
 
 	printf("using words per minute = $wpm\n");
 
-	$timeout = 60000 / $wpm;
-	$gtk_timer = Glib::Timeout->add(2000,\&set_text);
-
+	
+	# set up window and quit callbacks
 	$window = Gtk2::Window->new;
-	$window->signal_connect(delete_event => \&quit);
-	$window->signal_connect(destroy =>  \&quit);
+	$window->signal_connect(delete_event => \&button_quit);
+	$window->signal_connect(destroy =>  \&button_quit);
 	$window->set_border_width(10);
 
+	# quit button
 	$quit_button = Gtk2::Button->new("Quit");
-	$quit_button->signal_connect(clicked => \&quit, $window);
-	$quit_button->show;
+	$quit_button->signal_connect(clicked => \&button_quit, $window);
 
+	# backward button
 	$back_button = Gtk2::Button->new(" << ");
-	$back_button->signal_connect(clicked => \&back, $window);
-	$back_button->show;
+	$back_button->signal_connect(clicked => \&button_back, $window);
 
+	# forward button
 	$forward_button = Gtk2::Button->new(" >> ");
-	$forward_button->signal_connect(clicked => \&forward, $window);
-	$forward_button->show;
+	$forward_button->signal_connect(clicked => \&button_forward, $window);
 
-	$pause_button = Gtk2::Button->new(" || ");
-	$pause_button->signal_connect(clicked => \&pause, $window);
-	$pause_button->show;
+	# pause button
+	$pause_button = Gtk2::Button->new(" |> ");
+	$pause_button->signal_connect(clicked => \&button_pause, $window);
 
+	# faster button
+	$faster_button = Gtk2::Button->new(" + ");
+	$faster_button->signal_connect(clicked => \&button_faster, $window);
+
+	# slower button
+	$slower_button = Gtk2::Button->new(" - ");
+	$slower_button->signal_connect(clicked => \&button_slower, $window);
+
+	# text label, showing the actual speed
+	$gtk_speed_label = Gtk2::Label->new();
+	$gtk_speed_label->set_markup("WPM: $wpm");
+
+	# text label, showing the actual word
 	$gtk_text = Gtk2::Label->new();
-	$gtk_text->show;
+	for ($i = 0 ; $i < $word_width; ++$i) {
+		$word .= " ";
+	}
+	$gtk_text->set_markup($span_black_open.$word.$span_close);
 	
-	$gtk_sentence_text = Gtk2::Label->new();
-	$gtk_sentence_text->show;
 
-	$vbox = Gtk2::VBox->new(FALSE, 10);
+	# text label, showing the current sentence
+	$gtk_sentence_text = Gtk2::Label->new();
+	$gtk_sentence_text->set_markup("sentence nr: ");
+
+	# horizontal box for the control buttons
 	$hbox = Gtk2::HBox->new(FALSE, 10);
-	$hbox->pack_start($back_button, FALSE, FALSE, 0);
 	$hbox->pack_start($pause_button, FALSE, FALSE, 0);
+	$hbox->pack_start($back_button, FALSE, FALSE, 0);
 	$hbox->pack_start($forward_button, FALSE, FALSE, 0);
+	$hbox->pack_start(Gtk2::VSeparator->new(), FALSE, FALSE, 4);
+	$hbox->pack_start($slower_button, FALSE, FALSE, 0);
+	$hbox->pack_start($faster_button, FALSE, FALSE, 0);
+	$hbox->pack_start($gtk_speed_label, FALSE, FALSE, 0);
+	$hbox->pack_start(Gtk2::VSeparator->new(), FALSE, FALSE, 4);
 	$hbox->pack_start($gtk_sentence_text, FALSE, FALSE, 0);
+
+	# vertical box for the rest
+	$vbox = Gtk2::VBox->new(FALSE, 10);
 	$vbox->pack_start($hbox,FALSE,FALSE,4);
 	$vbox->pack_start(Gtk2::HSeparator->new(),FALSE,FALSE,4);
 	$vbox->pack_start($gtk_text, FALSE, TRUE, 5);
 	$vbox->pack_start(Gtk2::HSeparator->new(),FALSE,FALSE,4);
 	$vbox->pack_start($quit_button, FALSE, FALSE, 0);
-	$vbox->show;
 	$window->add($vbox);
 
+	# show window and start gtk main
 	$window->show_all;
 	Gtk2->main;
 
@@ -262,6 +348,10 @@ sub main
 
 main();
 
+
+################
+# help and man #
+################
 
 __END__
 
